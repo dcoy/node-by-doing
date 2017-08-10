0001f53693deb9ddebcfa3e3e24070af0543893c
David Coy <david@obsidianspork.io>
2017-08-09 22:10:44 -0500

    Change if/else to switch

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,27 +3,27 @@ console.log('Starting app.js...');
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
-
 const notes = require('./notes.js');
 
 const argv = yargs.argv
+let command = process.argv[2];
 
-var command = process.argv[2]
-console.log(`Command: ${command}`);
-console.log('Yargs:', argv)
+console.log('Command: ', command);
+console.log('Yargs: ', argv);
 
-if (command === 'add') {
-  console.log('Adding new note.');
-  notes.addNote(argv.title, argv.body);
-} else if(command === 'list') {
-  notes.getAll();
-  console.log('Listing all notes');
-} else if (command === 'read') {
-  console.log('Getting individual note.');
-  notes.getNote();
-} else if (command === 'remove') {
-  console.log('Removing note.')
-  notes.removeNote();
-} else {
-  console.log('Command not recognized.');
+switch (command) {
+  case 'add':
+    notes.addNote(argv.title, argv.body);
+    break;
+  case 'list':
+    notes.getAll();
+    break;
+  case 'read':
+    notes.getNote();
+    break;
+  case 'remove':
+    notes.removeNote();
+    break;
+  default:
+    console.log('Command not recognized.');
 }
